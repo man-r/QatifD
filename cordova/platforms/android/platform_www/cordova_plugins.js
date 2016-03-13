e39250006302a6f28da2bcc000cc8e594b84f7a3
man-r <man-r@github.com>
2016-03-13 09:35:44 +0300

    first commit adding freelancer theme and google login

---
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+        "id": "cordova-plugin-googleplus.GooglePlus",
+        "clobbers": [
+            "window.plugins.googleplus"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-googleplus": "4.0.8"
+};
+// BOTTOM OF METADATA
+});
